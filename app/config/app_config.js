02d2f00e1d7e0e1f02c8a5842ad2ec873778eb77
Ruben <rubalv96@gmail.com>
2020-04-30 18:03:32 +0200

    Moving some images to their root directory

--- app/config/app_config.js
+++ app/config/app_config.js
@@ -3,7 +3,7 @@ let GLOBAL_CONFIG_DEVELOPMENT = {
   // Nombre de la prueba
   title:"El misterio de Cleopatra",
   // Imagen de fondo
-  imageBackground:"./assets/images/puzzle/egipto_fondo.jpg", // imagen de fondo
+  imageBackground:"./assets/images/egipto_fondo.jpg", // imagen de fondo
   opacityBackground:"0.5", // opacidad de la imagen (por defecto sin opacidad)
 
   // Sonidos (por defecto, no hay música)
@@ -15,16 +15,16 @@ let GLOBAL_CONFIG_DEVELOPMENT = {
   // Dimensiones del puzzle
   M:3, // numero de columnas del puzzle (requerido)
   N:2, // numero de filas del puzzle (requerido)
-  fake_pieces:0,
+  fake_pieces:6,
 
   // Reverse mode (defecto -> false)
   reverseMode:true,
 
   // Timer
-  // time:"210", // tiempo en segundos para resolver el puzzle
+  time:"210", // tiempo en segundos para resolver el puzzle
 
   // Zoom
-  zoomMode:false, // activar modo zoom
+  zoomMode:true, // activar modo zoom
   zoomFactor:5, // factor de ampliación
 
   // Tiempo mínimo exigido para leer instrucciones (en segundos)
@@ -33,13 +33,13 @@ let GLOBAL_CONFIG_DEVELOPMENT = {
   // Mensaje inicial
   initialMessage:"El Antiguo Egipto está repleto de misterios. Las actividades cotidianas, las creencias, las leyendas y las vivencias son reflejados en el arte egipcio y esconde grandes incógnitas. Nos hemos adentrado en la residencia de Cleopatra y hemos encontrado un conjunto de piezas que pueden esconder un gran secreto. Los soldados han ido a buscar agua al Nilo para el baño diario de la faraona y tenemos 3 minutos y medio antes de que lleguen a los aposentos de Cleopatra. ¿Nos ayudas a resolverlo?", // mensaje inicial de bienvenida
   initialMessagePrint:"(mensaje configurable por el autor del recurso)", // mensaje inicial de bienvenida para impresión
-  initialImage:"./assets/images/puzzle/egipto_inicial.svg", // foto inicial de bienvenida
+  initialImage:"./assets/images/egipto_inicial.svg", // foto inicial de bienvenida
 
   // Mensaje final
   endMessageSuccess:"Has dado con uno de los papiros más importantes del Antiguo Egipto así como el símbolo del Ankh, el símbolo egipcio de la vida. Estamos más cerca de averiguar las grandes incógnitas de Cleopatra. ¡Enhorabuena!", // mensaje de exito
   endMessageFail:"Parece que eso no nos sirve para seguir investigando los misterios de Egipto.", // mensaje de fallo
-  endImageSuccess:"./assets/images/puzzle/egipto_inicial.svg", // imagen de exito
-  endImageFail:"./assets/images/puzzle/egipto_fallo.png", // imagen de fallo
+  endImageSuccess:"./assets/images/egipto_inicial.svg", // imagen de exito
+  endImageFail:"./assets/images/egipto_fallo.png", // imagen de fallo
 
   // Escapp configuraciones
   escapp:{
