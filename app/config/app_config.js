c608ffeb58d8531e6ff6f498ff5a4c6e5cc83f79
Ruben <rubalv96@gmail.com>
2020-05-03 18:01:22 +0200

    New example. Anatomia ocular

--- app/config/app_config.js
+++ app/config/app_config.js
@@ -1,21 +1,21 @@
 let GLOBAL_CONFIG_DEVELOPMENT = {
 
   // Nombre de la prueba
-  title:"El misterio de Cleopatra",
+  title:"Anatomía ocular",
   // Imagen de fondo
-  imageBackground:"./assets/images/egipto_fondo.jpg", // imagen de fondo
-  opacityBackground:"0.5", // opacidad de la imagen (por defecto sin opacidad)
+  imageBackground:"./assets/images/ojo_fondo.png", // imagen de fondo
+  opacityBackground:"0.3", // opacidad de la imagen (por defecto sin opacidad)
 
   // Sonidos (por defecto, no hay música)
-  backgroundMusic:"./assets/sounds/backgroundMusic.mp3", // https://www.fesliyanstudios.com/royalty-free-music/downloads-c/mysterious-music/7
-  successMusic:"./assets/sounds/successMusic.mp3", // http://soundbible.com/1003-Ta-Da.html
-  failureMusic:"./assets/sounds/failureMusic.mp3", // http://soundbible.com/1830-Sad-Trombone.html
-  volume:1, // Volumen de 0 a 1 (defecto 1)
+  // backgroundMusic:"./assets/sounds/backgroundMusic.mp3", // https://www.fesliyanstudios.com/royalty-free-music/downloads-c/mysterious-music/7
+  // successMusic:"./assets/sounds/successMusic.mp3", // http://soundbible.com/1003-Ta-Da.html
+  // failureMusic:"./assets/sounds/failureMusic.mp3", // http://soundbible.com/1830-Sad-Trombone.html
+  // volume:1, // Volumen de 0 a 1 (defecto 1)
 
   // Dimensiones del puzzle
   M:3, // numero de columnas del puzzle (requerido)
-  N:2, // numero de filas del puzzle (requerido)
-  fake_pieces:6,
+  N:3, // numero de filas del puzzle (requerido)
+  fake_pieces:0,
 
   // Reverse mode (defecto -> false)
   reverseMode:true,
@@ -31,15 +31,15 @@ let GLOBAL_CONFIG_DEVELOPMENT = {
   timeToReadInstructions:10,
 
   // Mensaje inicial
-  initialMessage:"El Antiguo Egipto está repleto de misterios. Las actividades cotidianas, las creencias, las leyendas y las vivencias son reflejados en el arte egipcio y esconde grandes incógnitas. Nos hemos adentrado en la residencia de Cleopatra y hemos encontrado un conjunto de piezas que pueden esconder un gran secreto. Los soldados han ido a buscar agua al Nilo para el baño diario de la faraona y tenemos 3 minutos y medio antes de que lleguen a los aposentos de Cleopatra. ¿Nos ayudas a resolverlo?", // mensaje inicial de bienvenida
+  initialMessage:"¿Serás capaz de resolver los misterios de la anatomía ocular?", // mensaje inicial de bienvenida
   initialMessagePrint:"(mensaje configurable por el autor del recurso)", // mensaje inicial de bienvenida para impresión
-  initialImage:"./assets/images/egipto_inicial.svg", // foto inicial de bienvenida
+  // initialImage:"./assets/images/egipto_inicial.svg", // foto inicial de bienvenida
 
   // Mensaje final
-  endMessageSuccess:"Has dado con uno de los papiros más importantes del Antiguo Egipto así como el símbolo del Ankh, el símbolo egipcio de la vida. Estamos más cerca de averiguar las grandes incógnitas de Cleopatra. ¡Enhorabuena!", // mensaje de exito
-  endMessageFail:"Parece que eso no nos sirve para seguir investigando los misterios de Egipto.", // mensaje de fallo
-  endImageSuccess:"./assets/images/egipto_inicial.svg", // imagen de exito
-  endImageFail:"./assets/images/egipto_fallo.png", // imagen de fallo
+  endMessageSuccess:"¡Has dado con la solución!", // mensaje de exito
+  endMessageFail:"Parece que eso no nos sirve para seguir investigando los misterios de la anatomía ocular.", // mensaje de fallo
+  // endImageSuccess:"./assets/images/egipto_inicial.svg", // imagen de exito
+  // endImageFail:"./assets/images/egipto_fallo.png", // imagen de fallo
 
   // Escapp configuraciones
   escapp:{
--- app/config/config_crop.js
+++ app/config/config_crop.js
@@ -1,10 +1,10 @@
 let GLOBAL_CONFIG_CROP = {
   // Imagenes de las piezas
-  image_solution_face:"./assets/images/puzzle/egipto_pintura.jpg", // imagen solución
-  image_solution_reverse:"./assets/images/puzzle/egipto_ank.jpg", // imagen solución reverso
-  image_fake1_face:"./assets/images/puzzle/egipto_altamira.jpg", // imagen señuelo cara
-  image_fake1_reverse:"./assets/images/puzzle/egipto_fatima.jpg", // imagen señuelo reverso
-  solution: "364975031758053978296812543065",
+  image_solution_face:"./assets/images/puzzle/anatomia_ojo.jpg", // imagen solución
+  image_solution_reverse:"./assets/images/puzzle/anatomia_ojo_2.jpg", // imagen solución reverso
+  // image_fake1_face:"./assets/images/puzzle/ojohumano.jpg", // imagen señuelo cara
+  // image_fake1_reverse:"./assets/images/puzzle/ojohumano.jpg", // imagen señuelo reverso
+  // solution: "364975031758053978296812543065",
 };
 
 module.exports = GLOBAL_CONFIG_CROP;
